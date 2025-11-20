52b71e92392acfa93bcaee2a60f17e01e070d98c
Nikhi7 <118648042+Nikhil-MR2001@users.noreply.github.com>
2025-11-20 15:22:58 +0530

    Updated HomeController.java

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello from Rakhi Parashar!!";
-    }
 
+@RequestMapping("/")
+String home() {
+    return "Hello from Rakhi Parashar!";
 }
+
